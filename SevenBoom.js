de3ad1932407d7add973d2a28ffde517504a0fad
tuseef123 <tossefashraf.292791@gmail.com>
2021-09-21 11:10:34 +0500

    sevenBoom

---
+++ SevenBoom.js
@@ -0,0 +1,38 @@
+// Create a function that takes an array of numbers and return "Boom!" if the digit 7 appears in the array. Otherwise, return "there is no 7 in the array".
+
+// Examples
+// sevenBoom([1, 2, 3, 4, 5, 6, 7]) ➞ "Boom!"
+// 7 contains the number seven.
+
+// sevenBoom([8, 6, 33, 100]) ➞ "there is no 7 in the array"
+// None of the items contain 7 within them.
+
+// sevenBoom([2, 55, 60, 97, 86]) ➞ "Boom!"
+// 97 contains the number seven.
+const sevenBoom = (arr) => {
+  let statement = "there is no 7 in the array";
+  arr.forEach((element) => {
+    element = element.toString();
+    if (element.length === 1) {
+      if (element == 7) {
+        statement = "Boom!";
+      }
+    } else if (element.length > 1) {
+      element = element.split("");
+      // console.log(element);
+      for (let i = 0; i < element.length; i++) {
+        // console.log(typeof element[i]);
+        if (element[i] == 7) {
+          // console.log(typeof element[i]);
+          statement = "Boom!";
+        }
+      }
+    }
+  });
+  return statement;
+};
+
+console.log(sevenBoom([1, 2, 3, 4, 5, 6, 7]));
+// console.log(sevenBoom([1, 2, 4]));
+// console.log(sevenBoom([42, 76, 55, 44, 32]));
+// console.log(sevenBoom([2, 55, 60, 97, 86]));
